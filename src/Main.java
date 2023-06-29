2ee72e8ef5be3ee2a03659ad7062783fcedb1232
Alessandro Ferreira Paz Lima <alessandrofpl@gmail.com>
2023-06-29 12:57:39 -0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        final String user = "alessandrofpl";
+        final String pass = "admin";
+        String login, senha;
+        Boolean logado = false;
+        int tentativa =1;
+
+        Scanner entrada = new Scanner(System.in);
+
+        do {
+            System.out.println("Digite o usuário: ");
+            login = entrada.nextLine();
+            System.out.println("Digite a Senha: ");
+            senha = entrada.nextLine();
+            if (login.equals(user) && senha.equals(pass)) {
+                System.out.println("Login Realizado Com Sucesso!");
+                logado = true;
+
+            } else {
+                System.out.println("Verifique os Parametro!");
+                logado = false;
+                tentativa++;
+            }
+        }while (logado == false && tentativa <=3);
+        if (tentativa>=3){
+            System.out.println("Conta Bloqueada!");
+        }
+    }
+}
